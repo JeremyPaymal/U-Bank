855d6ee0951d77aaa309e818f4a5b16470d9524b
Jeremy Paymal <jeremy.paymal@gmail.com>
2022-09-19 15:11:57 -0400

    First Commit

---
+++ src/components/Signin/index.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Container, Form, FormButton, FormContent, FormH1, FormInput, FormLabel, FormWrap, Icon, Text } from './SigninElements';
+import Footer from '../Footer';
+
+const SignIn = () => {
+  return (
+    <>
+        <Container>
+            <FormWrap>
+                <Icon to="/">U-Bank</Icon>
+                <FormContent>
+                    <Form action="#">
+                        <FormH1>Sign in to your account</FormH1>
+                        <FormLabel htmlFor='for'>Email</FormLabel>
+                        <FormInput type='email' required />
+                        <FormLabel htmlFor='for'>Password</FormLabel>
+                        <FormInput type='password' />
+                        <FormButton type="submit">Continue</FormButton>
+                        <Text>Forgot password</Text>
+                    </Form>
+                </FormContent>
+            </FormWrap>
+        </Container>
+        <Footer />
+    </>
+  )
+}
+
+export default SignIn;
